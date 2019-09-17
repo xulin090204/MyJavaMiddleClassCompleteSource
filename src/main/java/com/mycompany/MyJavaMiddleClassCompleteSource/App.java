dff888bd8c9f3abb42bea3e99818b51351683b40
xulin090204 <54882428+xulin090204@users.noreply.github.com>
2019-09-17 15:40:07 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/MyJavaMiddleClassCompleteSource/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.MyJavaMiddleClassCompleteSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
